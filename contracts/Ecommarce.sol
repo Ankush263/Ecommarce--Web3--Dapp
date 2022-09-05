85b722a7695759480c652be2b3687d9046a43cf0
Ankush263 <ankushbanik263@gmail.com>
2022-09-05 12:57:07 +0530

    update

--- contracts/Ecommarce.sol
+++ contracts/Ecommarce.sol
@@ -9,13 +9,17 @@ contract Ecommarce {
     string desc;
     uint price;
     uint productId;
-    uint stocks;
+    uint stocks;    // How many products has stocked
     address buyer;
     address payable seller;
     bool delevered;  
   }
 
-  mapping(address=>mapping(uint => uint)) productsNumber;   // mapping(address=>mapping(id=>no. of product)) ie. no[address][id]=numbers
+  mapping(address=>mapping(uint => uint)) public productsNumber;   // mapping(address=>mapping(id=>no. of product)) ie. no[address][id]=numbers
+
+  mapping(address => string) public deliveryLocation;    // where the item should delivered
+
+  mapping(uint => address) public customers;
 
   uint public deliveryDays;   // How many days need to delevered the product
   uint public listPrice;    // Listing price of a product into the market
@@ -28,6 +32,38 @@ contract Ecommarce {
   constructor() {
     manager = payable(msg.sender);
     listPrice = 0.01 ether;
+  }
+
+  function registerProduct(string memory _title, string memory _desc, uint _price, uint _stocks) public {
+
+    require(_price > 0, "Price should be greater then zero");
+    Product memory tempProduct;
+    tempProduct.title = _title;
+    tempProduct.desc = _desc;
+    tempProduct.price = _price * (10 ** 18);
+    tempProduct.stocks = _stocks;
+    tempProduct.seller = payable(msg.sender);
+    tempProduct.productId = count;
+    products.push(tempProduct);
+    count++;
+  
+  }
+
+  function buy(uint _productId, string memory _deliveryAddress, uint _numberOfProducts) public payable {
+
+    require(products[_productId-1].price == msg.value, "Please  pay the exact price");
+    require(products[_productId-1].seller != msg.sender, "Seller not be the buyer");
+    require(_numberOfProducts > 0, "Please select how many products you want to buy");
+    require(products[_productId-1].stocks >= _numberOfProducts, "You reach the product's stock limit");
+    products[_productId-1].buyer = msg.sender;
+    deliveryLocation[msg.sender] = _deliveryAddress;
+    customers[_productId] = msg.sender;
+    productsNumber[msg.sender][_productId] = _numberOfProducts;
+
+  }
+
+  function delivery() public {
+
   }
 
 
