2aa0b217128d0e9e2d3590164bb5047dbd7b4f23
Ankush263 <ankushbanik263@gmail.com>
2022-09-08 11:37:14 +0530

    set up next js client

---
+++ client/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className="app">
+      <h1>Hello Ankush</h1>
+    </div>
+  )
+}
+
+export default Home
