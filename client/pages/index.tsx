3faec56d438f17778680a87e648191133d19cf5f
Ankush263 <ankushbanik263@gmail.com>
2022-09-08 23:38:58 +0530

    Add

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,11 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import WellcomePage from './components/WellcomePage'
 
 const Home: NextPage = () => {
   return (
     <div className="app">
-      <h1>Hello Ankush</h1>
+      <WellcomePage />
     </div>
   )
 }
