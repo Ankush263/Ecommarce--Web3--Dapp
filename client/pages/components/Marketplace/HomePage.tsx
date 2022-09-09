29c79f0ba8a379fba1df686f8f6d97b59b89e279
Ankush263 <ankushbanik263@gmail.com>
2022-09-09 22:22:11 +0530

    Add

---
+++ client/pages/components/Marketplace/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../NavBar'
+
+function HomePage() {
+  return (
+    <div>
+      <NavBar />
+      HomePage
+    </div>
+  )
+}
+
+export default HomePage
