33fc04ccd03be6b62f78d3e2a07290684cd6fe12
Ankush263 <ankushbanik263@gmail.com>
2022-09-09 22:21:25 +0530

    update config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,10 +8,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'background': "#373B44",
-        'background': "-webkit-linear-gradient(to right, #4286f4, #373B44)",
-        'background': "linear-gradient(to right, #4286f4, #373B44)"
-
-      }
+      },
     },
   },
   plugins: [],
