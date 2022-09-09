757891d246f4b50f3abf91b21ba67f457c46a3ea
Ankush263 <ankushbanik263@gmail.com>
2022-09-09 13:45:43 +0530

    update

--- client/pages/components/WellcomePage.jsx
+++ client/pages/components/WellcomePage.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import Button from '@mui/material/Button';
+import '../../tailwind.config'
 
 
 function WellcomePage() {
 
   const styles = {
-    screen: `bg-[url('/images/sale.jpg')] bg-cover bg-center h-screen w-screen bg-no-repeat`,
-    second: `h-screen w-screen flex justify-center items-center`,
-    contain: `w-screen h-screen flex flex-col justify-center items-center`,
+    screen: `bg-background w-screen h-screen`,
+    second: `bg-[url('/images/cart.png')] h-screen w-screen flex flex-col justify-center items-center bg-no-repeat`,
     wallet: `bg-slate-300/[.2] shadow-2xl border-white-900/75 w-60 h-40 rounded-3xl flex flex-col justify-center items-center mb-10`,
     pages: `bg-slate-300/[.2] shadow-2xl border-white-900/75 w-6/12 h-60 rounded-3xl flex gap-20 justify-center items-center`,
+
   }
 
   return (
     <div className={styles.screen}>
       <div className={styles.second}>
-        <div className={styles.contain}>
-          <div className={styles.wallet}>
-            <span className='font-bold text-blue-700 text-lg'>Connect to the Metamask</span>
+        <div className={styles.wallet}>
+          <span className='font-bold text-slate-50 text-lg'>Connect to the Metamask</span>
             <Button variant="contained" className='bg-gradient-to-r from-sky-500 to-indigo-500'>
               <img src="/images/Metamask.png" className='w-10' />
               Connect
@@ -33,12 +33,9 @@ function WellcomePage() {
               <img src="/images/shopping.PNG" className='w-20' />
             </Button>
           </div>
-        </div>
       </div>
     </div>
   )
 }
 
 export default WellcomePage
-
-//h-screen text-black flex justify-center items-center 
