28b179992d0c4550c02649a939306f3b30d477c5
Ankush263 <ankushbanik263@gmail.com>
2022-09-10 19:20:05 +0530

    update

--- client/pages/components/Marketplace/HomePage.tsx
+++ client/pages/components/Marketplace/HomePage.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import NavBar from '../NavBar'
+import NavBar from './NavBarMarket'
+import SideBar from './SideBar'
+import Marketplace from './Marketplace'
 
 function HomePage() {
   return (
-    <div>
-      <NavBar />
-      HomePage
+    <div className='grid grid-cols-6 grid-flow-row'>
+      <div className="row-span-3">
+        <SideBar />
+      </div>
+      <div className="col-span-5 row-span-1">
+        <NavBar />
+      </div>
+      <div className="row-span-2 col-span-5">
+        <Marketplace />
+      </div>
     </div>
   )
 }
