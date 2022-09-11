fa970c852d8aa0a484151675075ff900f6236f3a
Ankush263 <ankushbanik263@gmail.com>
2022-09-11 11:38:34 +0530

    update

---
+++ client/pages/components/Marketplace/Marketplace.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Marketplace() {
+  return (
+    <div className='border-8 min-h-full mb-60'>
+      Marketplace
+      {/* <img src='/images/ecommerce-logo.png' className='border-8 w-4/12 h-3/6 mr-48 p-0'/>
+      <img src='/images/ecommerce-logo.png' className='border-8 w-4/12 h-3/6 mr-48 p-0'/>
+      <img src='/images/ecommerce-logo.png' className='border-8 w-4/12 h-3/6 mr-48 p-0'/>
+      <img src='/images/ecommerce-logo.png' className='border-8 w-4/12 h-3/6 mr-48 p-0'/>
+      <img src='/images/ecommerce-logo.png' className='border-8 w-4/12 h-3/6 mr-48 p-0'/>
+      <img src='/images/ecommerce-logo.png' className='border-8 w-4/12 h-3/6 mr-48 p-0'/>
+      <img src='/images/ecommerce-logo.png' className='border-8 w-4/12 h-3/6 mr-48 p-0'/>
+      <img src='/images/ecommerce-logo.png' className='border-8 w-4/12 h-3/6 mr-48 p-0'/>
+      <img src='/images/ecommerce-logo.png' className='border-8 w-4/12 h-3/6 mr-48 p-0'/>
+      <img src='/images/ecommerce-logo.png' className='border-8 w-4/12 h-3/6 mr-48 p-0'/> */}
+    </div>
+  )
+}
+
+export default Marketplace
