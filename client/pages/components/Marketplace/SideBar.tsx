0ac30881b0a2f7ec9e0aa6e64bff7841a94b787d
Ankush263 <ankushbanik263@gmail.com>
2022-09-12 09:50:08 +0530

    update

--- client/pages/components/Marketplace/SideBar.tsx
+++ client/pages/components/Marketplace/SideBar.tsx
@@ -1,15 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
+import PersonIcon from '@mui/icons-material/Person';
 
 function SideBar() {
 
   const styles = {
-    sidebar: `border-8 h-screen flex flex-col justify-start items-center gap-10`,
+    sidebar: `h-screen flex flex-col justify-start items-center gap-10`,
     top: ``,
-    topBox: `border-1 w-full h-60 rounded-2xl bg-gradient-to-r from-sky-500 to-indigo-500 flex flex-col justify-around items-center`,
+    topBox: `w-full h-60 rounded-2xl bg-gradient-to-r from-sky-500 to-indigo-500 flex flex-col justify-around items-center shadow-2xl border-white-900/75 cursor-pointer`,
     wallet: `flex flex-col bg-slate-200/[.2] shadow-2xl border-white-900/75 rounded-2xl p-2`,
-    bottom: ``,
+    bottom: `w-full h-1/2 flex flex-col justify-around items-center`,
     accountInfo:`flex text-lg font-semibold`,
+    sideWallet: `flex flex-col justify-center items-center`,
   }
 
   return (
@@ -29,7 +31,20 @@ function SideBar() {
         </div>
       </div>
       <div className={styles.bottom}>
-        {/* <img src="/images/ecommerce-logo.png" className='w-20' /> */}
+        <div className={styles.sideWallet}>
+          <img src="/images/Metamask.png" className='w-20' />
+          <span>930x....9302</span>
+        </div>
+        <div className={styles.sideWallet}>
+          <img src="/images/eth.png" className='w-8' />
+          <span>0.983923</span>
+        </div>
+        <div className={styles.sideWallet}>
+          <PersonIcon fontSize='large' />
+        </div>
+        <div className={styles.sideWallet}>
+          <img src='/images/ecommerce-logo.png' className='w-16 shadow-2xl border-white-900/75 rounded-full' />
+        </div>
       </div>
     </div>
   )
