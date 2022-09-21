8c2c39dddc01cf7293daa44fd804248734539b71
Ankush263 <ankushbanik263@gmail.com>
2022-09-21 12:59:07 +0530

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
+
+const PRIVATE_KEY: any = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
