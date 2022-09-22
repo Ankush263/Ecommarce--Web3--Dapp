3af60cba23efcc942ec0764d14ac9342a7cb51fd
Ankush263 <ankushbanik263@gmail.com>
2022-09-22 18:22:57 +0530

    update

---
+++ client/pages/components/Marketplace/ItemCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ItemCard(props: any) {
+
+  const styles = {
+    cardBox: ``,
+  }
+
+  return (
+    <div className={styles.cardBox}>
+
+    </div>
+  )
+}
+
+export default ItemCard
