376fc5d75c2d6013252393a635d49c689940d1d0
Ankush263 <ankushbanik263@gmail.com>
2022-09-24 00:06:35 +0530

    update

--- client/pages/components/Marketplace/ItemCard.tsx
+++ client/pages/components/Marketplace/ItemCard.tsx
@@ -3,15 +3,20 @@ import React from 'react'
 function ItemCard(props: any) {
 
   const styles = {
-    cardBox: `border-2 w-32 h-44`,
-    img: `w-full h-5/6`
+    cardBox: `w-36 h-56 flex flex-col justify-between items-center`,
+    imgBox: `w-full h-44 bg-slate-300/[.5] shadow-2xl border-white-900/75 rounded-xl`,
+    img: `w-full h-full border-4 border-orange-600 rounded-xl`,
   }
 
   return (
     <div className={styles.cardBox}>
-      <div className={styles.img}>
-        {/* <img src={props.data.img} /> */}
-        
+      <div className={styles.imgBox}>
+        <img src={props.data.img} className={styles.img} />
+      </div>
+      <span className='text-black text-sm font-semibold'>{props.data.title}</span>
+      <div className="flex justify-center items-center">
+        <span className='text-black text-sm font-semibold ml-5'>{props.data.price}</span>
+        <img src="/images/eth2.png" className='w-10 m-0 p-0' />
       </div>
     </div>
   )
