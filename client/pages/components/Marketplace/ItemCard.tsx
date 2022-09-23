ca09971168a40a745d925bf6e0cdca9613a353f8
Ankush263 <ankushbanik263@gmail.com>
2022-09-23 21:45:55 +0530

    update

--- client/pages/components/Marketplace/ItemCard.tsx
+++ client/pages/components/Marketplace/ItemCard.tsx
@@ -3,12 +3,16 @@ import React from 'react'
 function ItemCard(props: any) {
 
   const styles = {
-    cardBox: ``,
+    cardBox: `border-2 w-32 h-44`,
+    img: `w-full h-5/6`
   }
 
   return (
     <div className={styles.cardBox}>
-
+      <div className={styles.img}>
+        {/* <img src={props.data.img} /> */}
+        
+      </div>
     </div>
   )
 }
