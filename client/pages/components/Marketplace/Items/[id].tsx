370da96b6ed157f4e4a0ed15f2dba6e4ed739299
Ankush263 <ankushbanik263@gmail.com>
2022-09-25 23:09:04 +0530

    update

--- client/pages/components/Marketplace/Items/[id].tsx
+++ client/pages/components/Marketplace/Items/[id].tsx
@@ -4,12 +4,20 @@ import SideBar from '../SideBar'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import Button from '@mui/material/Button';
+import Link from 'next/link';
 
 function id() {
 
   const router = useRouter()
 
   const data = router.query
+
+  const items = {
+    img: data.img,
+    title: data.title,
+    price: data.price,
+    id: data.id
+  }
 
   const styles = {
     page: `min-w-screen min-h-screen flex justify-center items-center`,
@@ -22,7 +30,8 @@ function id() {
     mid: `min-w-52 h-64 mt-36 rounded-xl flex flex-col justify-center items-center`,
     right: `mr-2 w-36 h-10 flex justify-around items-center`,
     box: `w-8/12 h-2/3 flex flex-col justify-around  bg-slate-200/[.2] shadow-2xl border-white-900/75 rounded-xl p-2 border-4 mt-20 mr-20 border-orange-500`,
-    desc: `text-slate-600 font-semibold text-xl ml-2`
+    desc: `text-slate-600 font-semibold text-xl ml-2`,
+    btn: `block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`,
   }
 
   return (
@@ -51,7 +60,13 @@ function id() {
             <span className={styles.desc}>Owner: {data.seller}</span>
             <span className={styles.desc}>Price: {Number(data.price) / 10 ** 18}eth only</span>
             <div className="w-full flex justify-center items-center">
-              <Button variant="contained" className='w-8/12 bg-orange-500'>Buy</Button>
+              <Link href={{
+                pathname: '/components/Marketplace/Buy',
+                query: items
+              }}>
+                <Button variant="contained" className='w-8/12 bg-orange-500'>Buy</Button>
+              </Link>
+              {/* <Button variant="contained" className={styles.btn} data-modal-toggle="authentication-modal">Buy</Button> */}
             </div>
           </div>
 
