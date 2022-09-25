7e4c03bb8b7141c89fba5fc38e1da58a8509e312
Ankush263 <ankushbanik263@gmail.com>
2022-09-25 15:24:33 +0530

    update

--- client/pages/components/Marketplace/Items/[id].tsx
+++ client/pages/components/Marketplace/Items/[id].tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import SideBar from '../SideBar'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import Button from '@mui/material/Button';
 
 function id() {
 
@@ -16,11 +17,12 @@ function id() {
     mainBox: `w-full h-screen mr-2`,
     // top: `h-2/6 w-full flex justify-between items-start bg-slate-200/[.2] shadow-2xl border-white-900/75 `,
     top: `ml-2 h-2/6 w-full flex justify-between items-start shadow-2xl border-white-900/75 bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-xl`,
-    bottom : `w-full h-full border-4 flex justify-center items-center`,
+    bottom : `w-full h-full flex justify-center items-center`,
     left: `cursor-pointer`,
     mid: `min-w-52 h-64 mt-36 rounded-xl flex flex-col justify-center items-center`,
     right: `mr-2 w-36 h-10 flex justify-around items-center`,
-    box: `w-8/12 h-full border-4 mt-96 flex flex-col`,
+    box: `w-8/12 h-2/3 flex flex-col justify-around  bg-slate-200/[.2] shadow-2xl border-white-900/75 rounded-xl p-2 border-4 mt-20 mr-20 border-orange-500`,
+    desc: `text-slate-600 font-semibold text-xl ml-2`
   }
 
   return (
@@ -35,7 +37,7 @@ function id() {
           </div>
           <div className={styles.mid}>
             <img src={`${data.img}`} className='rounded-xl w-52 h-52 border-4 bg-slate-200/[.2] shadow-2xl border-orange-500 ' />
-            <span className='text-black font-semibold text-xl'>Title: {data.title}</span>
+            <span className='text-black font-semibold text-xl'>{data.title}</span>
           </div>
           <div className={styles.right}>
             <span>9999.983</span>
@@ -44,11 +46,15 @@ function id() {
         </div>
         <div className={styles.bottom}>
           <div className={styles.box}>
-            <span>Stocks:</span>
-            <span>Description:</span>
-            <span>Owner:</span>
-            <span>Price:</span>
+            <span className={styles.desc}>Only {data.stocks} pices left</span>
+            <span className={styles.desc}>Description: {data.desc}</span>
+            <span className={styles.desc}>Owner: {data.seller}</span>
+            <span className={styles.desc}>Price: {Number(data.price) / 10 ** 18}eth only</span>
+            <div className="w-full flex justify-center items-center">
+              <Button variant="contained" className='w-8/12 bg-orange-500'>Buy</Button>
+            </div>
           </div>
+
         </div>
       </div>
     </div>
