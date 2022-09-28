f88f21de4c087be5103b1970e1eb3e8fdb471fd4
Ankush263 <ankushbanik263@gmail.com>
2022-09-28 15:24:03 +0530

    update

--- client/pages/components/Marketplace/myProducts/Products.tsx
+++ client/pages/components/Marketplace/myProducts/Products.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 
-function products() {
+function products(props: any) {
 
   const styles = {
-    productBox: `w-full h-48 flex justify-between items-center`,
+    productBox: `w-full h-48 flex justify-between items-center mb-5`,
     imgBox: `w-48 h-48 border-4 rounded-xl ml-8`,
     rightBox: `w-80 h-48 mr-8 flex justify-center items-center`,
   }
@@ -12,13 +12,10 @@ function products() {
   return (
     <div className={styles.productBox}>
       <div className={styles.imgBox}></div>
-      <span className='text-2xl text-black text-bold'>Only 7 days left</span>
+      <span className='text-2xl text-black text-bold'>Order delivered in 7 Days</span>
       <div className={styles.rightBox}>
-        <div className="w-full flex justify-between items-center">
-          <Button variant="contained" color="error" >
-            Cancell Order
-          </Button>
-          <Button variant="contained" className='w-6/12' color="success">
+        <div className="w-full flex justify-end items-center">
+          <Button variant="contained" className='w-6/12 bg-green-700' color="success">
             Delivered
           </Button>
         </div>
