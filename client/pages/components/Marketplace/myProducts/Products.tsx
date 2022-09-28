205ca67d12c5d984ef47f5bb140e0a05748d5cc0
Ankush263 <ankushbanik263@gmail.com>
2022-09-28 15:49:14 +0530

    update

--- client/pages/components/Marketplace/myProducts/Products.tsx
+++ client/pages/components/Marketplace/myProducts/Products.tsx
@@ -5,14 +5,16 @@ function products(props: any) {
 
   const styles = {
     productBox: `w-full h-48 flex justify-between items-center mb-5`,
-    imgBox: `w-48 h-48 border-4 rounded-xl ml-8`,
+    imgBox: `w-48 h-48 rounded-xl ml-8`,
     rightBox: `w-80 h-48 mr-8 flex justify-center items-center`,
   }
 
   return (
     <div className={styles.productBox}>
-      <div className={styles.imgBox}></div>
-      <span className='text-2xl text-black text-bold'>Order delivered in 7 Days</span>
+      <div className={styles.imgBox}>
+        <img src={props.data.img} className='w-full h-full rounded-xl' />
+      </div>
+      <span className='text-2xl text-black text-bold'>Order delivered in {(Number(props.data.deliveryEnd) - Number(props.data.deliveryStart)) / 86400} Days</span>
       <div className={styles.rightBox}>
         <div className="w-full flex justify-end items-center">
           <Button variant="contained" className='w-6/12 bg-green-700' color="success">
