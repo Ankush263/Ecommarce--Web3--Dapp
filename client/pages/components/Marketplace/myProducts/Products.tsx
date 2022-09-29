63aeeb9907ffa13eeb20a103a7902bed88d4108b
Ankush263 <ankushbanik263@gmail.com>
2022-09-29 19:41:55 +0530

    update

--- client/pages/components/Marketplace/myProducts/Products.tsx
+++ client/pages/components/Marketplace/myProducts/Products.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
+import ABI from '../../../../../artifacts/contracts/Ecommarce.sol/Ecommarce.json';
+import { ethers } from 'ethers';
 
 function products(props: any) {
+
+  const deployAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+  const [click, setClick] = useState(false)
+
+  // if(props.data.delevered){
+  //   setClick(true)
+  // }
+
+  
+  
+  const delivered = async () => {
+  try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(deployAddress, ABI.abi, signer)
+      console.log(props.data.productId)
+      let transaction = await contract.delivery(props.data.productId)
+      await transaction.wait()
+      alert('Your Order is delivered to you!!!')
+      window.location.replace('/components/Marketplace/HomePage')
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const styles = {
     productBox: `w-full h-48 flex justify-between items-center mb-5`,
@@ -14,12 +42,14 @@ function products(props: any) {
       <div className={styles.imgBox}>
         <img src={props.data.img} className='w-full h-full rounded-xl' />
       </div>
-      <span className='text-2xl text-black text-bold'>Order delivered in {(Number(props.data.deliveryEnd) - Number(props.data.deliveryStart)) / 86400} Days</span>
+      {!props.data.delevered ? <span className='text-2xl text-black text-bold'>
+        Order delivered in {(Number(props.data.deliveryEnd) - Number(props.data.deliveryStart)) / 86400} Days
+      </span> : <span className='text-2xl text-black text-bold'>Thank you for shopping with us😊</span>}
       <div className={styles.rightBox}>
         <div className="w-full flex justify-end items-center">
-          <Button variant="contained" className='w-6/12 bg-green-700' color="success">
+          {!props.data.delevered ? <Button variant="contained" disabled={click} onClick={delivered} className='w-6/12 bg-green-700' color="success">
             Delivered
-          </Button>
+          </Button> : <span className='text-black text-2xl'>You have recived your product</span>}
         </div>
       </div>
     </div>
