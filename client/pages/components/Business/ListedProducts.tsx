d53af6f726c906e7c08278b0beb021d5f5a8d72e
Ankush263 <ankushbanik263@gmail.com>
2022-09-30 19:31:34 +0530

    update

--- client/pages/components/Business/ListedProducts.tsx
+++ client/pages/components/Business/ListedProducts.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 function ListedProducts() {
+
+  const styles = {
+    box: `w-11/12 h-52 bg-slate-300/[.5] shadow-2xl border-white-900/75 rounded-xl`,
+  }
+
   return (
-    <div>ListedProducts</div>
+    <div className={styles.box}>
+
+    </div>
   )
 }
 
