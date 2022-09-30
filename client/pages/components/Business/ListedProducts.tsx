79581a34d1bdade8ee9ecb9224e8ad8898d04485
Ankush263 <ankushbanik263@gmail.com>
2022-09-30 20:12:19 +0530

    update

--- client/pages/components/Business/ListedProducts.tsx
+++ client/pages/components/Business/ListedProducts.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@mui/material';
+import ABI from '../../../../artifacts/contracts/Ecommarce.sol/Ecommarce.json';
+import { ethers } from 'ethers';
+
+
 
 function ListedProducts() {
 
+  const deployAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+  const getItems = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const address = await signer.getAddress()
+      const contract = new ethers.Contract(deployAddress, ABI.abi, signer)
+      console.log("clicked")
+
+      console.log(await contract.getAllMyListedProducts())
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const styles = {
-    box: `w-11/12 h-52 bg-slate-300/[.5] shadow-2xl border-white-900/75 rounded-xl`,
+    box: `w-11/12 h-5/6 bg-slate-300/[.5] shadow-2xl border-white-900/75 rounded-xl mb-96`,
   }
 
   return (
     <div className={styles.box}>
-
+      <Button variant="contained" onClick={getItems}>click</Button>
     </div>
   )
 }
