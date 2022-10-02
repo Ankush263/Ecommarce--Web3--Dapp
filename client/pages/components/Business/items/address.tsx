9b502d07d6ba9293ed0a846e95970a586825220b
Ankush263 <ankushbanik263@gmail.com>
2022-10-02 08:54:50 +0530

    update

---
+++ client/pages/components/Business/items/address.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+
+function address() {
+  return (
+    <div>
+      <span>Address</span>
+      <Button>Click</Button>
+    </div>
+  )
+}
+
+export default address
