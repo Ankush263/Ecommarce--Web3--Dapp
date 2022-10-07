a8be4b6fdbdbe9b37f71e2ef373c187fb051a6cc
Ankush263 <ankushbanik263@gmail.com>
2022-10-07 22:42:54 +0530

    update

--- client/pages/components/Business/ListProduct.tsx
+++ client/pages/components/Business/ListProduct.tsx
@@ -113,7 +113,7 @@ function ListProduct() {
       <NavBar />
       <div className='flex flex-col justify-center items-center'>
         <div className={styles.space}>
-          <span className='w-11/12 h-1/6 text-4xl font-black'>
+          <div className='w-11/12 h-1/6 text-4xl font-black flex flex-col'>
             <span 
               className='ml-60 text-teal-900 font-serif'>
                 Welcome to the Web3.0 Ecommerce Business
@@ -122,7 +122,7 @@ function ListProduct() {
               className='ml-40 text-teal-900 font-serif'>
                 Here you can list any products with 0.01 eth each
             </span>
-          </span>
+          </div>
           <div className="w-11/12 h-screen flex justify-around">
             <div className={styles.left}>
               <div className={styles.box}>
