b0b0632f4129b3cca4c4796018865c26cefa2782
Ankush263 <ankushbanik263@gmail.com>
2022-10-08 20:27:42 +0530

    update

---
+++ client/pages/components/Business/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './NavBarBusiness'
+import ListProduct from './ListProduct'
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+      <ListProduct />
+    </div>
+  )
+}
+
+export default Home
