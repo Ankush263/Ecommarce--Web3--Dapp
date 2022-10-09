6ebf94f3d0aa148918a6e47ad763ed3348afbbde
Ankush263 <ankushbanik263@gmail.com>
2022-10-09 22:40:29 +0530

    update

--- client/pages/components/Marketplace/myProducts/Products.tsx
+++ client/pages/components/Marketplace/myProducts/Products.tsx
@@ -5,7 +5,7 @@ import { ethers } from 'ethers';
 
 function products(props: any) {
 
-  const deployAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  const deployAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
 
   const [click, setClick] = useState(false)
 
