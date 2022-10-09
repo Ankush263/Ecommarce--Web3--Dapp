3682727d5c37ef65b101f64012cf7e086d5163d6
Ankush263 <ankushbanik263@gmail.com>
2022-10-09 22:40:18 +0530

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,15 +8,15 @@ const RPC_URL: any = process.env.RPC_URL
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
-    // goerli: {
-    //   url: `https://eth-goerli.g.alchemy.com/v2/${process.env.API_KEY}`,
-    //   accounts: [PRIVATE_KEY]
-    // }
-    hardhat: {
-      // blockGasLimit: 0x1fffffffffffff,
-      chainId: 31337,
-      // allowUnlimitedContractSize: true,
-    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+    // hardhat: {
+    //   // blockGasLimit: 0x1fffffffffffff,
+    //   chainId: 31337,
+    //   // allowUnlimitedContractSize: true,
+    // },
   }
 };
 
