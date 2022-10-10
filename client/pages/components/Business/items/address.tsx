7494ed411c86a26e8eaee584f195d725f21d0718
Ankush263 <ankushbanik263@gmail.com>
2022-10-10 11:28:48 +0530

    update

--- client/pages/components/Business/items/address.tsx
+++ client/pages/components/Business/items/address.tsx
@@ -10,7 +10,7 @@ function Address(props: any) {
 
   const [delevered, setDeleverd] = useState(false)
 
-  const deployAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  const deployAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
   const showAddress = async (addr: any) => {
 
