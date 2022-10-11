ddc18a143d8f5ead3292b18bf48be6e78474718f
Ankush263 <ankushbanik263@gmail.com>
2022-10-11 15:16:35 +0530

    update

--- client/pages/components/Marketplace/SideBar.tsx
+++ client/pages/components/Marketplace/SideBar.tsx
@@ -6,7 +6,7 @@ import artifacts from "../../../utils/Ecommarce.json";
 
 function SideBar() {
 
-  const deployedAddress: string = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+  const deployedAddress: string = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
   const ABI: any = artifacts.abi
 
   const [walletAddress, getWalletAddress] = useState('')
