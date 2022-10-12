6a525f376f9fffeb0577b174b3b521fa04b285c7
Ankush263 <ankushbanik263@gmail.com>
2022-10-12 22:05:40 +0530

    update

--- client/pages/components/Marketplace/Marketplace.tsx
+++ client/pages/components/Marketplace/Marketplace.tsx
@@ -26,7 +26,7 @@ function Marketplace() {
   const [data, updateData] = useState(sampleData)
   const [dataFatched, updateDataFatched] = useState(false)
 
-  const deployAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  const deployAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 
   const getAllData = async () => {
     
