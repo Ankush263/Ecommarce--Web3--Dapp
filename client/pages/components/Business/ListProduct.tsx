8f0ade494bd43dbf291929a30ce7e4905911cb24
Ankush263 <ankushbanik263@gmail.com>
2022-10-12 22:05:11 +0530

    update

--- client/pages/components/Business/ListProduct.tsx
+++ client/pages/components/Business/ListProduct.tsx
@@ -8,7 +8,7 @@ import ABI from '../../../../artifacts/contracts/Ecommarce.sol/Ecommarce.json'
 
 function ListProduct() {
 
-  const deployAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  const deployAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 
   const [uploadImg, setUploadImg] = useState('')
   const [productDesc, setProductDesc] = useState({ title: '', desc: '', price: 0, stock: 0, img: '' })
