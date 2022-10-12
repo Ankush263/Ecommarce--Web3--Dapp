4d82a1e6568a10cfd946d1a9223ce32c67c627fb
Ankush263 <ankushbanik263@gmail.com>
2022-10-12 22:05:31 +0530

    update

--- client/pages/components/Marketplace/Buy.tsx
+++ client/pages/components/Marketplace/Buy.tsx
@@ -7,7 +7,7 @@ import ABI from '../../../../artifacts/contracts/Ecommarce.sol/Ecommarce.json';
 
 function Buy() {
 
-  const deployAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  const deployAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
   const [address, setAddress] = useState('')
   const [numberItems, setNumberItems] = useState(1)
   const [total, setTotal] = useState(1)
