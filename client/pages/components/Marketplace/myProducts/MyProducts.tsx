7fce5163acb9694a8868cc55874e4a52abd449d9
Ankush263 <ankushbanik263@gmail.com>
2022-10-12 22:05:51 +0530

    update

--- client/pages/components/Marketplace/myProducts/MyProducts.tsx
+++ client/pages/components/Marketplace/myProducts/MyProducts.tsx
@@ -21,7 +21,7 @@ function MyProducts() {
   ]
 
 
-  const deployAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  const deployAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
   const [data, setData] = useState(sampleData)
   const [fatch, setFatch] = useState(false)
 
