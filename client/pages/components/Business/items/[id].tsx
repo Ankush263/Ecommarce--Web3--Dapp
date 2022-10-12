3ea1472066c13f71400119c39c2c4b5f1cf9a6ae
Ankush263 <ankushbanik263@gmail.com>
2022-10-12 22:05:19 +0530

    update

--- client/pages/components/Business/items/[id].tsx
+++ client/pages/components/Business/items/[id].tsx
@@ -16,7 +16,7 @@ function id() {
   const [disable, setDisable] = useState(false)
   const [id, setId] = useState(0)
 
-  const deployAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  const deployAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 
   const test = async () => {
     setDisable(true)
