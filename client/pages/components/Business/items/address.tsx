8f7712f911069fc3fd878aa2f8a2b4e147bd7650
Ankush263 <ankushbanik263@gmail.com>
2022-10-12 22:05:24 +0530

    update

--- client/pages/components/Business/items/address.tsx
+++ client/pages/components/Business/items/address.tsx
@@ -10,7 +10,7 @@ function Address(props: any) {
 
   const [delevered, setDeleverd] = useState(false)
 
-  const deployAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  const deployAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 
   const showAddress = async (addr: any) => {
 
@@ -19,7 +19,8 @@ function Address(props: any) {
       const signer = provider.getSigner()
       const contract = new ethers.Contract(deployAddress, ABI.abi, signer)
 
-      let sendAddress = await contract.deliveryLocation(addr)
+      // let sendAddress = await contract.deliveryLocation(addr)
+      let sendAddress = await contract.deliveryLocation(props.id, addr)
 
       swal(sendAddress);
 
