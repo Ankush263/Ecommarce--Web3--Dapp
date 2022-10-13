dd9a29d24972674dd5bec722472af3e022d4da40
Ankush263 <ankushbanik263@gmail.com>
2022-10-13 20:59:11 +0530

    update

--- client/pages/components/Marketplace/Buy.tsx
+++ client/pages/components/Marketplace/Buy.tsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Button from '@mui/material/Button';
 import { ethers } from 'ethers';
-import ABI from '../../../../artifacts/contracts/Ecommarce.sol/Ecommarce.json';
+// import ABI from '../../../../artifacts/contracts/Ecommarce.sol/Ecommarce.json';
+import ABI from '../../../utils/Ecommarce.json';
 
 
 function Buy() {
 
-  const deployAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+  const deployAddress = "0x6cA0AC66ed28b00c2bbae46a0a003f04a006983e"
   const [address, setAddress] = useState('')
   const [numberItems, setNumberItems] = useState(1)
   const [total, setTotal] = useState(1)
