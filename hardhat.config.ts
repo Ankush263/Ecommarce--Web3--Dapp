676a4de51fbf145ac15e1939bb05ed158373b06f
Ankush263 <ankushbanik263@gmail.com>
2022-12-02 15:03:51 +0530

    Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,21 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 require('dotenv').config();
 
-const PRIVATE_KEY: any = process.env.PRIVATE_KEY;
-const RPC_URL: any = process.env.RPC_URL
+// const PRIVATE_KEY: any = process.env.PRIVATE_KEY;
+// const RPC_URL: any = process.env.RPC_URL
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
-    goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.API_KEY}`,
-      accounts: [PRIVATE_KEY]
-    }
-    // hardhat: {
-    //   // blockGasLimit: 0x1fffffffffffff,
-    //   chainId: 31337,
-    //   // allowUnlimitedContractSize: true,
+    // goerli: {
+    //   url: `https://eth-goerli.g.alchemy.com/v2/${process.env.API_KEY}`,
+    //   accounts: [PRIVATE_KEY]
     // },
+    hardhat: {
+      // blockGasLimit: 0x1fffffffffffff,
+      chainId: 31337,
+      // allowUnlimitedContractSize: true,
+    },
   }
 };
 
